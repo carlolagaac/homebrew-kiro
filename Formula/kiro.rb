d9378a346927175ae9d8f55ae9bf855952b0279a
carlo <carlo>
2026-03-21 19:16:13 +0800

    fix(kiro): use Dir[*] since Homebrew strips top-level tarball directory

--- Formula/kiro.rb
+++ Formula/kiro.rb
@@ -6,7 +6,7 @@ class Kiro < Formula
   sha256 "639c82dbc86a566e6b0bf4f3d721cb524f4d3047ac2d0a79bd879de2da7f00fe"
 
   def install
-    libexec.install Dir["Kiro/*"]
+    libexec.install Dir["*"]
     bin.write_exec_script libexec/"bin/kiro"
   end
 
